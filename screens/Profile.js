1669a714f72cefd0e020135cd21f568400238fe9
THE-UNKNOWN-CREATOR <70652146+THE-UNKNOWN-CREATOR@users.noreply.github.com>
2021-11-22 18:21:24 +0530

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,163 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Switch, SafeAreaView } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import firebase from 'firebase';
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      isEnabled: false,
+      profile_image: '',
+      name: "",
+      light_theme: true
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  async fetchUser() 
+  { 
+    let theme, name, image; 
+    await firebase.
+    database().
+    ref("/users/" + firebase.auth().currentUser.uid)
+    .on("value", function (snapshot) 
+      { 
+        theme = snapshot.val().current_theme; name = `${snapshot.val().first_name} ${snapshot.val().last_name}`;
+        image = snapshot.val().profile_picture;
+      }); 
+        this.setState(
+          { 
+            light_theme: theme === "light" ? true : false,
+            isEnabled: theme === "light" ? false : true, 
+            name: name, 
+            profile_image: image 
+          }); 
+    }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+    this.fetchUser();
+  }
+
+  toggleSwitch(){
+    const prevState = this.state.isEnabled;
+    const theme = (!this.state.theme) ? 'dark' : 'light';
+
+    var updates = {}
+
+    updates['/users/' + firebase.auth().currentUser().uid + "/current_theme"] = theme;
+    firebase.database().ref().update(updates);
+    this.setState({isEnabled: !prevState, light_theme: prevState});
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />
+    } else {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} >
+            <View style={styles.appTitle}>
+              <View style={styles.appIcon}>
+                <Image style={styles.iconImage} source={require('../assets/logo.png')}></Image>
+              </View>
+              <View style={styles.appTitleTextContainer}>
+                <Text style={styles.appTitleText}>Story Telling App</Text>
+              </View>
+              <Image style={styles.appIcon} source={{uri:this.state.profile_image}}></Image>
+            </View>
+            <Text>{this.state.name}</Text>
+            <Switch style=
+                    {{ 
+                        transform: [{ scaleX: 1.3 }, 
+                        { scaleY: 1.3 }] 
+                    }} 
+                    trackColor={{ false: "#767577", true: "white" }} 
+                    thumbColor={this.state.isEnabled ? "#ee8249" : "#f4f3f4"} 
+                    ios_backgroundColor="#3e3e3e" 
+                    onValueChange={() => this.toggleSwitch()} 
+                    value={this.state.isEnabled} 
+            />
+            <Text>Dark Theme</Text>
+          </SafeAreaView>
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  screenContainer: {
+    flex: 0.85
+  },
+  profileImageContainer: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  profileImage: {
+    width: RFValue(140),
+    height: RFValue(140),
+    borderRadius: RFValue(70)
+  },
+  nameText: {
+    color: "white",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans",
+    marginTop: RFValue(10)
+  },
+  themeContainer: {
+    flex: 0.2,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: RFValue(20)
+  },
+  themeText: {
+    color: "white",
+    fontSize: RFValue(30),
+    fontFamily: "Bubblegum-Sans",
+    marginRight: RFValue(15)
+  }
+});
